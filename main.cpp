4fd73af5f71bbc2647a0c7c872ccba3e1147704e
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-05-27 14:29:55 +0200

    Create small working program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "Brainfuck.hpp"
+#include <iostream>
+#include <sstream>
+
+int main() {
+  std::stringstream ss;
+  std::string line;
+  while (std::getline(std::cin, line))
+    ss << line;
+
+  Brainfuck bf{};
+  auto compiled = bf.compile(ss.str());
+  std::cout << compiled << std::endl;
+
+  return 0;
+}
