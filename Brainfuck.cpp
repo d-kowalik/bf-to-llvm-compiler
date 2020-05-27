fb83d75d980118ab002a6c186bed92b1883b5c8e
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-05-27 16:26:04 +0200

    Add separate load command for readability

--- Brainfuck.cpp
+++ Brainfuck.cpp
@@ -4,6 +4,8 @@
 #include <stack>
 #include <utility>
 
+#include "commands/load.hpp"
+
 const std::string HEADER =
     "target triple = \"x86_64-pc-linux-gnu\"\n"
     "@tape = private unnamed_addr global [2000 x i8] zeroinitializer\n"
@@ -28,23 +30,21 @@ std::string Brainfuck::compile(Code const &code) const {
     char c = code[i];
     switch (c) {
     case '.':
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
-      ss << "%" << (unused_symbol + 1) << " = load i8, i8* %" << unused_symbol
-         << "\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
+      ss << load(unused_symbol + 1, "i8", unused_symbol);
       ss << "call i8 @putchar(i8 %" << (unused_symbol + 1) << ")\n";
       unused_symbol += 3; // due to call returning a value
       break;
     case ',':
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
       ss << "%" << (unused_symbol + 1) << " = call i8 @getchar()\n";
       ss << "store i8 %" << (unused_symbol + 1) << ", i8* %" << unused_symbol
          << ", align 1\n";
       unused_symbol += 2;
       break;
     case '+':
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
-      ss << "%" << (unused_symbol + 1) << " = load i8, i8* %" << unused_symbol
-         << "\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
+      ss << load(unused_symbol + 1, "i8", unused_symbol);
       ss << "%" << (unused_symbol + 2) << " = add i8 %" << (unused_symbol + 1)
          << ", 1\n";
       ss << "store i8 %" << (unused_symbol + 2) << ", i8* %" << unused_symbol
@@ -52,9 +52,8 @@ std::string Brainfuck::compile(Code const &code) const {
       unused_symbol += 3;
       break;
     case '-':
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
-      ss << "%" << (unused_symbol + 1) << " = load i8, i8* %" << unused_symbol
-         << "\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
+      ss << load(unused_symbol + 1, "i8", unused_symbol);
       ss << "%" << (unused_symbol + 2) << " = sub i8 %" << (unused_symbol + 1)
          << ", 1\n";
       ss << "store i8 %" << (unused_symbol + 2) << ", i8* %" << unused_symbol
@@ -62,7 +61,7 @@ std::string Brainfuck::compile(Code const &code) const {
       unused_symbol += 3;
       break;
     case '>':
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
       ss << "%" << (unused_symbol + 1) << " = ptrtoint i8* %" << unused_symbol
          << " to i64\n";
       ss << "%" << (unused_symbol + 2) << " = add i64 %" << (unused_symbol + 1)
@@ -73,7 +72,7 @@ std::string Brainfuck::compile(Code const &code) const {
       unused_symbol += 4;
       break;
     case '<':
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
       ss << "%" << (unused_symbol + 1) << " = ptrtoint i8* %" << unused_symbol
          << " to i64\n";
       ss << "%" << (unused_symbol + 2) << " = sub i64 %" << (unused_symbol + 1)
@@ -87,9 +86,8 @@ std::string Brainfuck::compile(Code const &code) const {
       ss << "br label %for" << unused_loop_symbol << ".body\n";
       ss << "for" << unused_loop_symbol << ".body:\n";
       brackets.push(unused_loop_symbol);
-      ss << "%" << unused_symbol << " = load i8*, i8** %tape_ptr\n";
-      ss << "%" << (unused_symbol + 1) << " = load i8, i8* %" << unused_symbol
-         << "\n";
+      ss << load(unused_symbol, "i8*", "tape_ptr");
+      ss << load(unused_symbol + 1, "i8", unused_symbol);
       ss << "%" << (unused_symbol + 2) << " = icmp eq i8 %"
          << (unused_symbol + 1) << ", 0\n";
       ss << "br i1 %" << (unused_symbol + 2) << ", label %for"
