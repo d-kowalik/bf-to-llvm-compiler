eb083fe1ff1e57c48d98a535c7d46f7c448924d9
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-06-17 14:39:20 +0200

    Complete HandleRead

--- LLVMCompiler.cpp
+++ LLVMCompiler.cpp
@@ -1,7 +1,9 @@
 #include "LLVMCompiler.hpp"
 
+#include "instructions/llvm/CallGetchar.hpp"
 #include "instructions/llvm/CallPutchar.hpp"
 #include "instructions/llvm/LoadInstruction.hpp"
+#include "instructions/llvm/StoreInstruction.hpp"
 
 using namespace variables;
 using namespace instructions::llvm;
@@ -12,4 +14,12 @@ void LLVMCompiler::HandlePrint() {
   Emit<LoadInstruction>(dereferenced_tape, tape_ptr);
   Emit<LoadInstruction>(cell_value, dereferenced_tape);
   Emit<CallPutchar>(cell_value);
+}
+
+void LLVMCompiler::HandleRead() {
+  auto dereferenced_tape = std::make_shared<LLVMCountedVariable>(Type::Int8Ptr);
+  auto input = std::make_shared<LLVMCountedVariable>(Type::Int8);
+  Emit<LoadInstruction>(dereferenced_tape, tape_ptr);
+  Emit<CallGetchar>(input);
+  Emit<StoreInstruction>(input, dereferenced_tape);
 }
