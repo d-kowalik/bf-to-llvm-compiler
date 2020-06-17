5b101114ccec40b2f777a80a4a05f6be7b63d7e4
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-06-17 14:19:33 +0200

    Add TypeToString static method

--- variables/LLVMVariable.cpp
+++ variables/LLVMVariable.cpp
@@ -4,4 +4,27 @@ namespace variables {
 LLVMVariable::LLVMVariable(Type type, std::string name)
     : Variable("%" + name, type) {}
 
+std::string LLVMVariable::TypeToString(Type type) {
+  switch (type) {
+  case Type::Int8:
+    return "i8";
+  case Type::Int8Ptr:
+    return "i8*";
+  case Type::Int8PtrPtr:
+    return "i8**";
+  case Type::Int32:
+    return "i32";
+  case Type::Int32Ptr:
+    return "i32*";
+  case Type::Int32PtrPtr:
+    return "i32**";
+    return "i64";
+  case Type::Int64Ptr:
+    return "i64*";
+  case Type::Int64PtrPtr:
+    return "i64**";
+  }
+  return "";
+}
+
 } // namespace variables
--- variables/LLVMVariable.hpp
+++ variables/LLVMVariable.hpp
@@ -6,5 +6,7 @@ namespace variables {
 class LLVMVariable : public Variable {
 public:
   LLVMVariable(Type type, std::string name);
+
+  static std::string TypeToString(Type type);
 };
 } // namespace variables
