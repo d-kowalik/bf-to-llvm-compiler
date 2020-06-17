1a2fd17053f5b14786f33fce29e49e8bc2694f43
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-06-17 14:31:02 +0200

    Add CallGetchar instruction

---
+++ instructions/llvm/CallGetchar.cpp
@@ -0,0 +1,16 @@
+#include "CallGetchar.hpp"
+
+#include <sstream>
+
+namespace instructions::llvm {
+
+CallGetchar::CallGetchar(VariablePtr destination) : destination{destination} {}
+
+std::string CallGetchar::Execute() {
+  std::stringstream ss;
+  ss << destination->GetName();
+  ss << " = call i8 @getchar()\n";
+  return ss.str();
+};
+
+} // namespace instructions::llvm
---
+++ instructions/llvm/CallGetchar.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "LLVMInstruction.hpp"
+
+namespace instructions::llvm {
+
+class CallGetchar : public LLVMInstruction {
+  VariablePtr destination;
+
+public:
+  CallGetchar(VariablePtr destination);
+
+  std::string Execute() override;
+}
+
+} // namespace instructions::llvm
