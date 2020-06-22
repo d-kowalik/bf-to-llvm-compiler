d21d451fe58f24d3ce7bf390a6d18d191ef1f71b
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-06-22 19:20:15 +0200

    Adjust main to use new code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include "Brainfuck.hpp"
 #include <iostream>
-#include <sstream>
 
-int main() {
+#include "CompilerManager.hpp"
+#include "LLVMCompiler.hpp"
+
+int main()
+{
   std::stringstream ss;
   std::string line;
   while (std::getline(std::cin, line))
     ss << line;
 
-  Brainfuck bf{};
-  auto compiled = bf.compile(ss.str());
+  CompilerManager manager{ss.str(), std::make_shared<LLVMCompiler>()};
+  auto compiled = manager.Compile();
   std::cout << compiled << std::endl;
 
   return 0;
