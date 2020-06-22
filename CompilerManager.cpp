755fbdb7ea703a8a27aad4e76ac466eec360c1dc
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-06-22 19:17:58 +0200

    Implement CompilerManager

---
+++ CompilerManager.cpp
@@ -0,0 +1,41 @@
+#include "CompilerManager.hpp"
+
+CompilerManager::CompilerManager(const std::string &code) : code{code} {};
+
+std::string CompilerManager::Compile()
+{
+  for (char c : code)
+  {
+    switch (c)
+    {
+    case '.':
+      compiler->HandlePrint();
+      break;
+    case ',':
+      compiler->HandleRead();
+      break;
+    case '+':
+      compiler->HandleIncrement();
+      break;
+    case '-':
+      compiler->HandleDecrement();
+      break;
+    case '>':
+      compiler->HandleMoveRight();
+      break;
+    case '<':
+      compiler->HandleMoveLeft();
+      break;
+    case '[':
+      compiler->HandleLoopBegin();
+      break;
+    case ']':
+      compiler->HandleLoopEnd();
+      break;
+    default:
+      break;
+    }
+  }
+
+  return compiler->Compile();
+}
---
+++ CompilerManager.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Compiler.hpp"
+
+#include <memory>
+#include <string>
+
+class CompilerManager
+{
+  std::shared_ptr<Compiler> compiler;
+  std::string code;
+
+public:
+  CompilerManager(const std::string &code);
+
+  std::string Compile();
+};
