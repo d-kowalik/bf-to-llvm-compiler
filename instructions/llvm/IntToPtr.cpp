8b676ea9d88239e94bd35143590afcac1f09b07a
Damian Kowalik <dev.damian.kowalik@gmail.com>
2020-06-22 18:16:16 +0200

    Add IntToPtr instuction

---
+++ instructions/llvm/IntToPtr.cpp
@@ -0,0 +1,19 @@
+#include "IntToPtr.hpp"
+
+using namespace variables;
+
+namespace instructions::llvm
+{
+
+  std::string IntToPtr::Execute()
+  {
+    std::stringstream ss;
+    ss << destination->GetName();
+    ss << " = inttoptr ";
+    ss << LLVMVariable::TypeToString(source->GetType());
+    ss << source->GetName();
+    ss << " to ";
+    ss << LLVMVariable::TypeToString(destination->GetType());
+    return ss.str();
+  }
+} // namespace instructions::llvm
